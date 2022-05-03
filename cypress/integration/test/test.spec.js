5121332240beae245e84d477bbf140baf6644093
kalinichev.ivan <kalinichev.ivan@geek.band>
2022-05-03 22:23:34 +0300

    add e2e tests

---
+++ cypress/integration/test/test.spec.js
@@ -0,0 +1,32 @@
+/// <reference types="Cypress" />
+
+
+import {cards} from "../../support/pages/cards";
+
+
+before(() => {
+  cy.visit('http://localhost:4200/')
+})
+
+it('Start page test', () => {
+  cards.checkPageTitle('Hello Angular')
+  cards.checkToggleCards()
+})
+
+it('First card', () => {
+  cards.checkTitleAndTime('1 Card 1', 'This is card number 1', 0)
+  cards.checkColorButton('0')
+  cards.checkTitleButton('Test title', '0', 1)
+})
+
+it('Second card', () => {
+  cards.checkTitleAndTime('2 This is card 2', 'This is card number 2',  1)
+  cards.checkColorButton('1')
+  cards.checkTitleButton('Test title', '1', 2)
+})
+
+it('Third card', () => {
+  cards.checkTitleAndTime('3 Last card!', 'This is card number 3',  2)
+  cards.checkColorButton(2)
+  cards.checkTitleButton('Test title', 2, 3)
+})
---
+++ cypress/support/pages/cards.js
@@ -0,0 +1,50 @@
+export class Cards {
+
+
+  checkPageTitle(text) {
+    cy.get('.container').get('h1').should('have.text', text)
+  }
+
+  checkToggleCards() {
+    cy.contains("Toggle Cards").click()
+    cy.get('p').contains('Cards are hidden!')
+    cy.contains("Toggle Cards").click()
+  }
+
+  checkTitleAndTime(h2_text, p_text, eq) {
+    const dayjs = require('dayjs');
+    const todaysDate = dayjs().format('DD MMM YYYY HH:mm')
+    cy.get('.card').eq(eq)
+      .get('h2').eq(eq).should('have.text', h2_text)
+      .get('p').eq(eq).should('have.text', p_text)
+      .get('strong').eq(eq).contains(todaysDate)
+  }
+
+  checkColorButton(eq) {
+    cy.get('.card').eq(eq)
+      .contains("Set Blue").click()
+      .get('p').eq(eq).should('have.css', 'color', 'rgb(0, 0, 255)')
+    cy.get('.card').eq(eq)
+      .contains("Set Green").click()
+      .get('p').eq(eq).should('have.css', 'color', 'rgb(0, 128, 0)')
+    cy.get('.card').eq(eq)
+      .contains("Set Red").click()
+      .get('p').eq(eq).should('have.css', 'color', 'rgb(255, 0, 0)')
+  }
+
+  checkTitleButton(text, eq, card_number) {
+    cy.get('.card').eq(eq)
+      .contains("Change title").click()
+      .get('h2').eq(eq).should('have.text', card_number + ' Title has been changed!')
+      .get('input').eq(eq).invoke('attr', 'ng-reflect-model').should('contain', 'Title has been changed!')
+    cy.get('.card').eq(eq)
+      .contains("Change title inline").click()
+      .get('h2').eq(eq).should('have.text', card_number + ' Inline title')
+      .get('input').eq(eq).invoke('attr', 'ng-reflect-model').should('contain', 'Inline title')
+    cy.get('.card').eq(eq)
+      .get('input').eq(eq).clear().type(text)
+  }
+
+}
+
+export const cards = new Cards()
